3cc30d0f7473af33d6c2976b45a7daacdabc9047
Bryce Hanscomb <bryce@brycehanscomb.com>
2019-11-18 15:50:33 +1100

    disable dark mode; add hash scrolling

--- common.js
+++ common.js
@@ -3,3 +3,11 @@ export const $$ = s => Array.from(document.querySelectorAll(s));
 
 export const body = $("body");
 export const title = $(".title");
+
+const url = new URL(window.location.href);
+
+if (url.hash.length) {
+    const targetEl = $('#' + url.hash.replace('#', ''));
+
+    window.scrollTo(x, targetEl.getBoundingClientRect().y);
+}
